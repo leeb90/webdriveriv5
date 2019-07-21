0c5a005d3d3f06309095e4a1d59bf74b8d1915d6
Luis Efrain Espinoza Banegas <lespinoza@Luiss-MacBook-Pro.local>
2019-07-21 11:10:32 -0600

    adding time validations

--- test/specs/calculator_timeerrormessages.spec.js
+++ test/specs/calculator_timeerrormessages.spec.js
@@ -1,9 +1,8 @@
 const expect= require('chai').expect;
 const CalculatorPage = require('../../calculator.pageobjects');
-const LotContent= require ('../../helpers/testdata').LotContent;
-const LotContentValues= require ('../../helpers/testdata').LotContentValues;
 describe('Parking Calculator Time Error Messages',()=>{
-    let ErrorMessage='ERROR! ENTER A CORRECTLY FORMATTED TIME';
+    let ErrorMessageFormat='ERROR! ENTER A CORRECTLY FORMATTED TIME';
+    let ErrorMessageTime='ERROR! YOUR EXIT DATE OR TIME IS BEFORE YOUR ENTRY DATE OR TIME';
     it('should check error message when invalid time is filled',()=>{
         CalculatorPage.open('http://adam.goucher.ca/parkcalc/');
         browser.pause(2000);
@@ -12,7 +11,7 @@ describe('Parking Calculator Time Error Messages',()=>{
         CalculatorPage.EntryDate.setValue('07/21/2020');
         CalculatorPage.ExitDate.setValue('07/21/2020');
         CalculatorPage.CalculateButton.click();
-        expect(CalculatorPage.CalculateCost.getText(),'No calculation should be done and error message should appear').to.equal(ErrorMessage);
+        expect(CalculatorPage.CalculateCost.getText(),'No calculation should be done and error message should appear').to.equal(ErrorMessageFormat);
     });
 
     it('should check time format',()=>{
@@ -27,7 +26,17 @@ describe('Parking Calculator Time Error Messages',()=>{
         let ExitValue= CalculatorPage.ExitTimeTextBox.getValue();
         expect(EntryValue.length, 'Time format for Entry time should consist of only 4 numbers').to.equal(5);
         expect(ExitValue.length, 'Time format for Exit time should consist of only 4 numbers').to.equal(5)
+    });
 
+    it('should check Exit time to be before entry time',()=>{
+        CalculatorPage.open('http://adam.goucher.ca/parkcalc/');
+        browser.pause(2000);
+        CalculatorPage.EntryTimeTextBox.setValue('03:00');
+        CalculatorPage.ExitTimeTextBox.setValue('02:00');
+        CalculatorPage.EntryDate.setValue('07/21/2020');
+        CalculatorPage.ExitDate.setValue('07/21/2020');
+        CalculatorPage.CalculateButton.click();
+        expect(CalculatorPage.CalculateCost.getText(),'No calculation should be done and error message should appear').to.equal(ErrorMessageTime);
     });
 
 });
