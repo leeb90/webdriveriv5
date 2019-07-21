246af7a999fcaa9acf4175957e66ca3438632225
Luis Efrain Espinoza Banegas <lespinoza@Luiss-MacBook-Pro.local>
2019-07-21 12:25:30 -0600

    functional regular expression to check HH:MM format

--- test/specs/calculator_timeerrormessages.spec.js
+++ test/specs/calculator_timeerrormessages.spec.js
@@ -4,6 +4,7 @@ describe('Parking Calculator Time Error Messages',()=>{
     let ErrorMessageFormat='ERROR! ENTER A CORRECTLY FORMATTED TIME';
     let ErrorMessageTime='ERROR! YOUR EXIT DATE OR TIME IS BEFORE YOUR ENTRY DATE OR TIME';
     let RegexTimeFormat= /^([01]\d|2[0-3]):?([0-5]\d)$/g;
+    let MatchBoolean= true;
     it('should check error message when invalid time is filled',()=>{
         CalculatorPage.open('http://adam.goucher.ca/parkcalc/');
         CalculatorPage.EntryTimeTextBox.setValue('fsfsdfsdf');
@@ -16,18 +17,19 @@ describe('Parking Calculator Time Error Messages',()=>{
 
     it('should check time format HH:MM',()=>{
         CalculatorPage.open('http://adam.goucher.ca/parkcalc/');
-        CalculatorPage.EntryTimeTextBox.setValue('32:00');
-        CalculatorPage.ExitTimeTextBox.setValue('13:00');
+        CalculatorPage.EntryTimeTextBox.setValue('765765');
+        CalculatorPage.ExitTimeTextBox.setValue('3543');
         CalculatorPage.EntryDate.setValue('07/21/2020');
         CalculatorPage.ExitDate.setValue('07/21/2020');
         CalculatorPage.CalculateButton.click();
         let EntryValue= CalculatorPage.EntryTimeTextBox.getValue();
         let ExitValue= CalculatorPage.ExitTimeTextBox.getValue();
-        let MatchFoundEntry= EntryValue.match(RegexTimeFormat);
-        console.log('Data is ' + MatchFoundEntry);
-       // let MatchFoundExit= ExitValue.match(RegexTimeFormat);
-        //expect(MatchFoundEntry[0], 'Time format for Entry time should consist of only 4 numbers and colon HH:MM').to.equal(5);
-        //expect(MatchFoundExit[0], 'Time format for Exit time should consist of only 4 numbers and colon HH:MM').to.equal(5);
+        let MatchFoundEntry = EntryValue.match(RegexTimeFormat);
+        let MatchFoundExit= ExitValue.match(RegexTimeFormat);
+        if (MatchFoundEntry===null || MatchFoundExit===null){
+             MatchBoolean=false;
+        }
+        expect(MatchBoolean, 'Time format for Entry time should consist of only 4 numbers and colon HH:MM').to.be.true;
     });
 
     it('should check Exit time to be before entry time',()=>{
